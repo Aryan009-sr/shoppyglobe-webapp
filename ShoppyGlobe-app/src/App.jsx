ad8825e7591df8c71dbbe1a6187fb91bde8203cc
Aryan009-sr <Prakharsingh9532@gmail.com>
2025-08-25 17:19:38 +0530

    Added lazy loading to multiple components

--- ShoppyGlobe-app/src/App.jsx
+++ ShoppyGlobe-app/src/App.jsx
@@ -1,22 +1,22 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import ProductList from './components/ProductList';
-import NotFoundPage from './components/NotFoundPage';
 import Footer from './components/footer';
-import Checkout from './components/Checkout';
 
 // Lazy load the components for code splitting
 const Cart = lazy(() => import('./components/Cart'));
+const ProductList = lazy(() => import ('./components/ProductList'));
 const ProductDetail = lazy(() => import('./components/ProductDetail'));
+const Checkout = lazy(() => import ('./components/Checkout'));
+const NotFoundPage = lazy(() => import('./components/NotFoundPage'))
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App flex flex-col min-h-screen">
         <Header />
-        <main>
-          <Suspense fallback={<div>Loading...</div>}>
+        <main className='flex-grow'>
+          <Suspense fallback={<div className='text-center py-8'>Loading...</div>}>
             <Routes>
               <Route element={<ProductList />} path="/" />
               <Route element={<Cart />} path="/cart" />
--- ShoppyGlobe-app/src/redux/cartSlice.js
+++ ShoppyGlobe-app/src/redux/cartSlice.js
@@ -26,16 +26,10 @@ const cartSlice = createSlice({
       }
     },
     decreaseQuantity: (state, action) => {
-      const itemId = action.payload;
-      const existingItem = state.items.find(item => item.id === itemId);
-      if (existingItem) {
-        if (existingItem.quantity === 1) {
-          // Remove the item from the cart if quantity is 1
-          state.items = state.items.filter(item => item.id !== itemId);
-        } else {
-          // Otherwise, just decrease the quantity
-          existingItem.quantity--;
-        }
+      const existingItem = state.items.find(item => item.id === action.payload);
+      // The quantity should not go below 1
+      if (existingItem && existingItem.quantity > 1) {
+        existingItem.quantity--;
       }
     },
   },
